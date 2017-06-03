5e9f058cfc1e6a27136d0b3c6e68d8045551c115
Graham Jones <graham@insilicodesigns.com>
2017-06-02 21:35:34 -0700

    rework core profiler into a class

--- launcher.py
+++ launcher.py
@@ -1,7 +1,7 @@
-from profilers.profiler_core import run_profiler as core_profiler
 from store_results import store_results
 from profilers.utils import get_sandbox
 from profilers.posix_extra import PosixExtraProfiler
+from profilers.posix_core import PosixCoreProfiler
 
 ## Entrace point - handlers for different environments
 
@@ -9,7 +9,8 @@ def lambda_handler(event, context):
     env = get_sandbox()
     
     # results = core_profiler()
-    results = PosixExtraProfiler.run()
+    # results = PosixExtraProfiler.run()
+    results = PosixCoreProfiler.run()
     
     results['sandbox'] = env
 
--- profilers/profiler_base.py
+++ profilers/profiler_base.py
@@ -1,7 +1,18 @@
+"""
+python-lambda-inspector
+General concept for now:
+    'lookups' is a dict that contains names of info
+    and maps them to the functions to determine that info.
+    'wrapper' is a function that takes no args and calls
+    the regular lambda start point for running the code locally.
+    Now with CI goodness in us-west-2.
+"""
+
 class Profiler(object):
 
+    """Main map table of items to post or store."""
     lookups = {}
     
-    @staticmethod
-    def run():
+    @classmethod
+    def run(cls):
         raise Exception("Implement this method!")
