502c28ea634840741faca5ff7bd527c6799b1fb2
Joel Ferrier <joel@ferrier.io>
2017-06-26 23:08:48 -0700

    port compatible unix_core lookups

--- profilers/windows_core.py
+++ profilers/windows_core.py
@@ -18,3 +18,71 @@ class WindowsCoreProfiler(Profiler):
 
     """Functions for specific data retreival."""
 
+    def get_release_version():
+        return platform.release()
+
+
+    def get_packages():
+        return [x[1] for x in pkgutil.iter_modules()]
+
+    def get_package_versions():
+        results = {}
+        for x in pkgutil.iter_modules():
+            try:
+                results[x[1]] = {
+                    'version': str(pkg_resources.get_distribution(x[1]).version)
+                }
+            except:
+                pass
+        return results
+
+    def get_package_count():
+        return len([x[1] for x in pkgutil.iter_modules()])
+
+    def get_timestamp():
+        return calendar.timegm(datetime.utcnow().utctimetuple())
+
+    def get_ipaddress():
+        # http://stackoverflow.com/questions/166506/finding-local-ip-addresses-using-pythons-stdlib/25850698#25850698
+        local_ip_address='0.0.0.0'
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            s.connect(('8.8.8.8', 53))
+            local_ip_address = s.getsockname()[0]
+        except Exception as e:
+            pass
+
+        return local_ip_address
+
+
+    lookups = {
+        "pwd":        get_pwd,
+        "release":    get_release_version,
+        "is_warm":    is_warm.is_warm,
+        "warm_since": is_warm.warm_since,
+        "warm_for":   is_warm.warm_for,
+        "package_count": get_package_count,
+        "packages":   get_packages,
+        "package_versions": get_package_versions,
+        "timestamp":  get_timestamp,
+        "ipaddress":  get_ipaddress,
+    }
+
+    @staticmethod
+    def jsonify_results(d):
+        if 'warm_since' in d:
+            d['warm_since'] = str(d['warm_since'])
+        if 'warm_for' in d:
+            d['warm_for'] = str(d['warm_for'])
+
+        return d
+
+    @classmethod
+    def run(cls):
+        res = make_result_dict(cls.lookups)
+
+        import pdb
+        # pdb.set_trace()
+        is_warm.mark_warm()
+
+        return cls.jsonify_results(res)
